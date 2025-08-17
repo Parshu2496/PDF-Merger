684449aa9058f8a1c565a5f2b1ba6797de727313
Prashant <prashantnrwd@gmail.com>
2025-08-17 22:43:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,20 +12,24 @@ const port = 3000
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"template/index.html"))
 })
-app.post('/merge',upload.array('pdfs',4), async (req, res,next)=> {
-  console.log(req.files)
-  const filePaths = req.files.map(file => path.join(__dirname, file.path));
-        // Call MergePdfs with the array of file paths
-        let d = await MergePdfs(filePaths);
-        // Delete the uploaded files
-  filePaths.forEach(filePath => {
-            fs.unlink(filePath, (err) => {
-                if (err) console.error(`Error deleting file: ${filePath}`, err);
-            });
+app.post('/merge', upload.array('pdfs', 4), async (req, res, next) => {
+    console.log(req.files);
+    const filePaths = req.files.map(file => path.join(__dirname, file.path));
+    
+    // Call MergePdfs with the array of file paths
+    let pdfFileName = await MergePdfs(filePaths);
+    
+    // Delete the uploaded files
+    filePaths.forEach(filePath => {
+        fs.unlink(filePath, (err) => {
+            if (err) console.error(`Error deleting file: ${filePath}`, err);
         });
-  res.redirect(`http://localhost:3000/static/${d}.pdf`)
-  // res.send({data: req.files})
-})
+    });
+    
+    // Send back the URL of the generated PDF
+    res.json({ downloadUrl: `http://localhost:3000/static/${pdfFileName}.pdf` });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
