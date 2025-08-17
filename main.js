73d342d34c136d422d07dd40e63f01e6ce8083a7
Prashant <prashantnrwd@gmail.com>
2025-08-17 23:42:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const multer = require('multer')
+const express = require('express');
+const path = require('path');
+const app = express();
+const multer = require('multer');
 const fs = require('fs');
-const {MergePdfs} = require('./merge')
+const { MergePdfs } = require('./merge');
 
-const upload = multer({dest: 'uploads/'})
-app.use('/static',express.static('public'))
-const port = 3000
+const upload = multer({ dest: 'uploads/' });
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"template/index.html"))
-})
-app.post('/merge',upload.array('pdfs',4), async (req, res,next)=> {
-  console.log(req.files)
+  res.sendFile(path.join(__dirname, "template/index.html"));
+});
+
+app.post('/merge', upload.array('pdfs', 4), async (req, res) => {
+  console.log(req.files);
   const filePaths = req.files.map(file => path.join(__dirname, file.path));
-        // Call MergePdfs with the array of file paths
-        let d = await MergePdfs(filePaths);
-        // Delete the uploaded files
+  
+  // Call MergePdfs with the array of file paths
+  const mergedPdfBuffer = await MergePdfs(filePaths);
+
+  // Delete the uploaded files
   filePaths.forEach(filePath => {
-            fs.unlink(filePath, (err) => {
-                if (err) console.error(`Error deleting file: ${filePath}`, err);
-            });
-        });
-  res.redirect(`http://localhost:3000/static/${d}.pdf`)
-  // res.send({data: req.files})
-})
+    fs.unlink(filePath, (err) => {
+      if (err) console.error(`Error deleting file: ${filePath}`, err);
+    });
+  });
+
+  // Set the appropriate headers to prompt the browser to open the PDF
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', 'inline; filename=merged.pdf');
+  res.send(mergedPdfBuffer); // Send the merged PDF buffer as the response
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
